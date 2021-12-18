3ccab4d68af58b8f14e5e012f13fce07fe5548dc
Lucas Souza <lucassouzatm@gmail.com>
2021-12-17 21:08:34 -0300

    Estudando .replace

---
+++ 9-replace.js
@@ -0,0 +1,7 @@
+const CPFSemFormatacao = 'cpf é 25684677037'
+
+const CPFFormatado = CPFSemFormatacao.replace('25684677037', '256.846.770-37') 
+
+console.log(CPFFormatado) 
+// O retorno será 'cpf é 256.846.770-37'
+
